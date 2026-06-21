500d6393e5c2fe64511f6c2fe0750299446d5f81
SAMAR QAYYUM <samar.qayyumwork@gmail.com>
2026-06-21 23:21:18 +0500

    Serve media files in production (temporary fallback for Railway)

--- citybuddy/urls.py
+++ citybuddy/urls.py
@@ -15,12 +15,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# In production (DEBUG=False) Django does not automatically serve media files.
+# For small apps on platforms without a separate static/media host (like this
+# Railway deployment), add a simple media serving view when DEBUG is False.
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
+
+# During development, the static() helper already adds media serving when DEBUG=True
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
